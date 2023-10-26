4a2cd3971a43fbedd6cd1faea902ad330fc1eca4
jamil noyda <jamil.noyda@hpe.com>
2023-10-26 16:35:01 +0530

    testing done it worked

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/jamilnoyda/go-package-testing
+
+go 1.20
+
+require github.com/jamilnoyda/go-server v0.0.0-20231025125135-5e2804eb8521
--- hello_world.go
+++ hello_world.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-func main()  {
-    fmt.Println("Hello World")
+	mymath "github.com/jamilnoyda/go-server/math"
+)
+
+func main() {
+	fmt.Println("Hello World")
+	fmt.Println(mymath.Add(2, 2))
 }
